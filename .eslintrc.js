f924fd8c8a70338ec0d3835291bfc27d64298d1b
eren.ozturk <erenozturk@gmail.com>
2021-04-10 17:18:04 +0300

    eslint & prettier config added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.js', '*.ts', '*.jsx', '*.tsx'],
+      rules: {
+        'jsx-quotes': 0,
+      },
+      'prettier/prettier': [
+        "error",
+        {
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  ],
 };
